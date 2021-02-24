4881e9d634adea5df51a28ea1584d03081519ad4
dannyr22 <dannyr2763@hotmail.com>
2021-02-24 20:13:02 +0000

    5. About section styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from './components/GlobalStyle'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <GlobalStyle/>
       <AboutUs />
     </div>
   );
