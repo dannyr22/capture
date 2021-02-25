c6aab7f7c4eac1370b7e988fdf892adce247d369
dannyr22 <dannyr2763@hotmail.com>
2021-02-25 18:24:21 +0000

    9. main heading transitions added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import home1 from '../img/home1.png'
-import { About, Description, Image, Hide} from '../styles'
+import { About, Description, Image, Hide } from '../styles';
+import { motion } from 'framer-motion';
 
 const AboutSection = () => {
-  return ( 
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: {opacity: 1, transition: {duration: 2}}
+  }
+
+  const container = {
+    hidden: { x: 100 },
+    show: {x: 0, transition: {duration: 0.75, ease: "easeOut", staggerChildren: 1, when: "afterChildren"}}
+  }
+
+  return (  
     <About >
       <Description>
-        <div className="title">
+        <motion.div
+          className="title"
+          variants={container}
+          animate="show"
+          initial="hidden">
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2
+              variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>your <span>dreams</span></h2>
+            <motion.h2
+              variants={titleAnim}>your <span>dreams</span> come</motion.h2>
           </Hide>
           <Hide>
-            <h2>come true.</h2>
+            <motion.h2
+              variants={titleAnim}>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
         <button>Contact us</button>
       </Description>
